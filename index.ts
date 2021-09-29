686324f2c5250e484b8a4ee80faa2ddc900b3d82
srinivas <s7varanasi@gmail.com>
2021-09-29 23:35:59 +0530

    test: mocha added

--- index.ts
+++ index.ts
@@ -1,12 +1,6 @@
-import Algo from "./src/files/index";
-
-const aes = new Algo()
-aes.encrypt("test.txt", "aes-192-cbc", "12345").then(() => {
-    aes.decrypt("test.txt.enc", "aes-192-cbc", "12345").catch((err) => console.log(err))
-}).catch((err) => console.log(err))
-
+import File from "./src/files/index";
 
 
 export default {
-    Algo
+    File
 }
--- src/files/index.ts
+++ src/files/index.ts
@@ -1,104 +1,140 @@
 import { scrypt, randomFill, createCipheriv, randomBytes, createDecipheriv, scryptSync } from "crypto";
 import { createReadStream, createWriteStream } from 'fs';
 import { pipeline } from "stream";
+import type {BinaryLike, ParameterReturn } from "../../types/index";
 
 interface AlgorithmInterface {
-    encrypt(path: string, algorithm: string, password: string, parameters?: Options): Promise<void | Array<Buffer | Uint8Array>>;
-    decrypt(path: string, algorithm: string, password: string, parameters?: Options): Promise<void>;
-    encryptSync(path: string, algorithm: string, password: string, parameters?: Options): void | Error;
+    encrypt(path: string, algorithm?: string, password?: string, parameters?: Options): Promise<ParameterReturn>;
+    decrypt(path: string, algorithm?: string, password?: string, parameters?: Options): Promise<void>;
+    encryptSync(path: string, algorithm: string, password: string, parameters?: Options): ParameterReturn | Error;
+    decryptSync(path: string, algorithm?: string, password?: string, parameters?: Options): void | Error;
 };
 
 interface Options {
-    salt: Buffer | Uint8Array | Uint8ClampedArray | Uint16Array | Uint32Array | Int8Array | Int16Array | Int32Array | BigUint64Array | BigInt64Array | Float32Array | Float64Array | string;
-    iv: Uint8Array | Uint8ClampedArray | Uint16Array | Uint32Array | Int8Array | Int16Array | Int32Array | BigUint64Array | BigInt64Array | Float32Array | Float64Array | string;
+    salt?: Buffer | BinaryLike,
+    iv?: BinaryLike
 }
 
-class Algorithms implements AlgorithmInterface{
-    private buffer: Buffer | null = null;
-    private iv: Uint8Array | null = null;
+class Encryptor implements AlgorithmInterface{
+    public buffer: Buffer | null = null;
+    public iv: BinaryLike | null = null;
+    public password: string | null = null;
+    public algorithm: string | null = null;
 
-
-    encrypt = (path: string, algorithm: string, password: string, parameters?: Options) => {
+    constructor(password?: string, algorithm?: string){
+        if(password) this.password = password;
+        if(algorithm) this.algorithm = algorithm;
+    }
+    encrypt = (path: string, algorithm?: string, password?: string, parameters?: Options) => {
         const options = parameters ? parameters as Options : null;
         if( options && options.iv && options.salt ){
-            return new Promise<void>((resolve, reject) => {
-                scrypt(password, options.salt, 24, (err: Error | null, key: Buffer): void => {
+            return new Promise<ParameterReturn>((resolve, reject) => {
+                scrypt(password ? password : this.password as string, options.salt as Buffer | BinaryLike, 24, (err: Error | null, key: Buffer): void => {
                     if(err) reject(err);
-                    const cipher = createCipheriv(algorithm, key, options.iv);
+                    const cipher = createCipheriv(algorithm ? algorithm : this.algorithm as string, key, options.iv as BinaryLike);
     
                     const input = createReadStream(path);
                     const output = createWriteStream(`${path}.enc`);
     
                     pipeline(input, cipher, output, (err: NodeJS.ErrnoException | null) => {
                         if(err) reject(err);
-                        resolve();
+                        resolve({
+                            salt: this.buffer as Buffer,
+                            iv: this.iv as BinaryLike
+                        });
                     });
                 })
             })
-        } else {
-            return new Promise<void>((resolve, reject) => {
-                if(!this.buffer) this.buffer = randomBytes(16);
-                scrypt(password, this.buffer, 24, (err: Error | null, key: Buffer): void => {
+        } else if(options && options.iv && !options.salt){
+            this.buffer = randomBytes(16);
+            return new Promise<ParameterReturn>((resolve, reject) => {
+                scrypt(password ? password : this.password as string, this.buffer as Buffer, 24, (err: Error | null, key: Buffer): void => {
+                    if(err) reject(err);
+                    const cipher = createCipheriv(algorithm ? algorithm : this.algorithm as string, key, options.iv as BinaryLike);
+    
+                    const input = createReadStream(path);
+                    const output = createWriteStream(`${path}.enc`);
+    
+                    pipeline(input, cipher, output, (err: NodeJS.ErrnoException | null) => {
+                        if(err) reject(err);
+                        resolve({
+                            salt: this.buffer as Buffer,
+                            iv: this.iv as BinaryLike
+                        });;
+                    });
+                })
+            })
+        } else if (options && !options.iv && options.salt){
+            return new Promise<ParameterReturn>((resolve, reject) => {
+                scrypt(password ? password : this.password as string, this.buffer as Buffer, 24, (err: Error | null, key: Buffer): void => {
                     if(err) reject(err);
                     randomFill(new Uint8Array(16), (err: Error | null, iv: Uint8Array) => {
                         if(err) reject(err);
-                        if(!this.iv) this.iv = iv;
-                        const cipher = createCipheriv(algorithm, key, iv);
+                        this.iv = iv;
+                        const cipher = createCipheriv(algorithm ? algorithm : this.algorithm as string, key, iv);
 
                         const input = createReadStream(path);
                         const output = createWriteStream(`${path}.enc`);
 
                         pipeline(input, cipher, output, (err: NodeJS.ErrnoException | null) => {
                             if(err) reject(err);
-                            resolve();
+                            resolve({
+                                salt: this.buffer as Buffer,
+                                iv: this.iv as BinaryLike
+                            });;
+                        });
+                    })
+                })
+            })
+        } else {
+            return new Promise<ParameterReturn>((resolve, reject) => {
+                this.buffer = randomBytes(16);
+                scrypt(password ? password : this.password as string, this.buffer, 24, (err: Error | null, key: Buffer): void => {
+                    if(err) reject(err);
+                    randomFill(new Uint8Array(16), (err: Error | null, iv: Uint8Array) => {
+                        if(err) reject(err);
+                        this.iv = iv;
+                        const cipher = createCipheriv(algorithm ? algorithm : this.algorithm as string, key, iv);
+
+                        const input = createReadStream(path);
+                        const output = createWriteStream(`${path}.enc`);
+
+                        pipeline(input, cipher, output, (err: NodeJS.ErrnoException | null) => {
+                            if(err) reject(err);
+                            resolve({
+                                salt: this.buffer as Buffer,
+                                iv: this.iv as BinaryLike
+                            });;
                         });
                     })
                 })
             })
         }
     };
-
  
-    decrypt = (path: string, algorithm: string, password: string, parameters?: Options) => {
+    decrypt = (path: string, algorithm?: string, password?: string, parameters?: Options) => {
         const options = parameters ? parameters as Options : null;
-        if(options && options.iv && options.salt){
-            return new Promise<void>((resolve, reject) => {
-                scrypt(password, options.salt, 24, (err: Error | null, key: Buffer): void => {
+        return new Promise<void>((resolve, reject) => {
+            scrypt(password ? password : this.password as string, (options && options.salt) ? options.salt : this.buffer as Buffer, 24, (err: Error | null, key: Buffer): void => {
+                if(err) reject(err);
+                const input = createReadStream(path);
+                const output = createWriteStream("text.txt");
+                const decipher = createDecipheriv(algorithm ? algorithm : this.algorithm as string, key, (options && options.iv) ? options.iv : this.iv as BinaryLike);
+
+                pipeline(input, decipher, output, (err: NodeJS.ErrnoException | null) => {
                     if(err) reject(err);
-                    const input = createReadStream(path);
-                    const output = createWriteStream("text.txt");
-                    const decipher = createDecipheriv(algorithm, key, options.iv);
-    
-                    pipeline(input, decipher, output, (err: NodeJS.ErrnoException | null) => {
-                        if(err) reject(err);
-                        resolve();
-                    })
+                    resolve();
                 })
             })
-        } else {
-            return new Promise<void>((resolve, reject) => {
-                scrypt(password, this.buffer as Buffer, 24, (err: Error | null, key: Buffer): void => {
-                    if(err) reject(err);
-                    const input = createReadStream(path);
-                    const output = createWriteStream("text.txt");
-                    const decipher = createDecipheriv(algorithm, key, this.iv);
-    
-                    pipeline(input, decipher, output, (err: NodeJS.ErrnoException | null) => {
-                        if(err) reject(err);
-                        resolve();
-                    })
-                })
-            })
-        } 
+        })
     };
 
-    encryptSync = (path: string, algorithm: string, password: string, parameters?: Options): void | Error => {
+    encryptSync = (path: string, algorithm?: string, password?: string, parameters?: Options): ParameterReturn | Error => {
         try{
-            const key = scryptSync(password, randomBytes(16), 24);
-            randomFill(new Uint8Array(16), (err: Error | null, iv: Uint8Array) => {
-                if(err) throw err;
-                if(!this.iv) this.iv = iv;
-                const cipher = createCipheriv(algorithm, key, iv);
+            const options = parameters ? parameters as Options : null;
+            if(options && options.salt && options.iv){
+                const key = scryptSync(password ? password : this.password as string, options.salt, 24);
+                const cipher = createCipheriv(algorithm ? algorithm : this.algorithm as string, key, options.iv);
 
                 const input = createReadStream(path);
                 const output = createWriteStream(`${path}.enc`);
@@ -106,11 +142,71 @@ class Algorithms implements AlgorithmInterface{
                 pipeline(input, cipher, output, (err: NodeJS.ErrnoException | null) => {
                     if(err) throw err;
                 });
-            })
+            } else if(options && options.iv && !options.salt){
+                this.buffer = randomBytes(16);
+                const key = scryptSync(password ? password : this.password as string, this.buffer as Buffer, 24);
+                const cipher = createCipheriv(algorithm ? algorithm : this.algorithm as string, key, options.iv);
+
+                const input = createReadStream(path);
+                const output = createWriteStream(`${path}.enc`);
+
+                pipeline(input, cipher, output, (err: NodeJS.ErrnoException | null) => {
+                    if(err) throw err;
+                });
+            } else if(options && options.salt && !options.iv){
+                const key = scryptSync(password ? password : this.password as string, options.salt, 24);
+                randomFill(new Uint8Array(16), (err: Error | null, iv: Uint8Array) => {
+                    if(err) throw err;
+                    this.iv = iv;
+                    const cipher = createCipheriv(algorithm ? algorithm : this.algorithm as string, key, iv);
+    
+                    const input = createReadStream(path);
+                    const output = createWriteStream(`${path}.enc`);
+    
+                    pipeline(input, cipher, output, (err: NodeJS.ErrnoException | null) => {
+                        if(err) throw err;
+                    });
+                });
+            } else {
+                this.buffer = randomBytes(16);
+                const key = scryptSync(password ? password : this.password as string, this.buffer, 24);
+                randomFill(new Uint8Array(16), (err: Error | null, iv: Uint8Array) => {
+                    if(err) throw err;
+                    this.iv = iv;
+                    const cipher = createCipheriv(algorithm ? algorithm : this.algorithm as string, key, iv);
+
+                    const input = createReadStream(path);
+                    const output = createWriteStream(`${path}.enc`);
+
+                    pipeline(input, cipher, output, (err: NodeJS.ErrnoException | null) => {
+                        if(err) throw err;
+                    });
+                })
+            }
+            return {
+                salt: this.buffer as Buffer,
+                iv: this.iv as BinaryLike
+            }
         } catch (err: unknown) {
             return err as Error
         }
     };
+
+    decryptSync = (path: string, algorithm?: string, password?: string, parameters?: Options): void | Error => {
+        try{
+            const key = scryptSync(password ? password : this.password as string, (parameters && parameters.salt) ? parameters.salt : this.buffer as Buffer, 24);
+            
+            const input = createReadStream(path);
+            const output = createWriteStream("text.txt");
+            const decipher = createDecipheriv(algorithm ? algorithm : this.algorithm as string, key, (parameters && parameters.iv) ? parameters.iv : this.iv as BinaryLike);
+
+            pipeline(input, decipher, output, (err: NodeJS.ErrnoException | null) => {
+                if(err) throw err;
+            })
+        } catch (err: unknown){
+            return err as Error;
+        }
+    }
 };
 
-export default Algorithms;
+export default Encryptor;
